647c8c79e387e95f4bddad74dca78a17468508c3
Monday <mangmalai_p@silpakorn.edu>
2023-09-16 20:08:17 +0700

    (add) Main Mechanics +moving player by hold down the mouse button to set the position then release the button to shoot in that direction.

---
+++ Assets/_SCRIPT/PlayerController.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+public class WhiteBallController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+    private bool isAiming = false;
+    private bool isMoving = false;
+
+    public float maxPower = 20f;
+    public float powerMultiplier = 5f;
+    public float stopThreshold = 0.1f; // Adjust this threshold for when the ball is considered stopped
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // Freeze rotation to prevent rolling
+        startPosition = transform.position;
+        targetPosition = startPosition;
+    }
+
+    void Update()
+    {
+        if (!isMoving)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Start aiming when the left mouse button is pressed
+                isAiming = true;
+            }
+
+            if (isAiming)
+            {
+                // Update the target position based on mouse input
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Plane tablePlane = new Plane(Vector3.up, startPosition);
+
+                float hitDistance;
+                if (tablePlane.Raycast(ray, out hitDistance))
+                {
+                    targetPosition = ray.GetPoint(hitDistance);
+                }
+
+                // Visualize the target position (you can remove this for the final game)
+                Debug.DrawLine(targetPosition, targetPosition + Vector3.up, Color.red);
+            }
+
+            if (Input.GetMouseButtonUp(0) && isAiming)
+            {
+                // Shoot the ball in the opposite direction when the left mouse button is released
+                Vector3 shootDirection = startPosition - targetPosition;
+                shootDirection.y = 0; // Ensure the shot stays in the same plane as the table
+
+                float power = Mathf.Clamp(shootDirection.magnitude * powerMultiplier, 0f, maxPower);
+
+                rb.AddForce(shootDirection.normalized * power, ForceMode.Impulse);
+
+                // Update the start position to the current position
+                startPosition = transform.position;
+
+                // Reset the aiming flag
+                isAiming = false;
+
+                // Indicate that the ball is moving
+                isMoving = true;
+            }
+        }
+        else
+        {
+            // Check if the ball has stopped moving
+            if (rb.velocity.magnitude < stopThreshold)
+            {
+                isMoving = false;
+                rb.freezeRotation = true; // Freeze rotation again
+            }
+            else
+            {
+                // Update the rotation to face the direction of movement
+                Quaternion lookRotation = Quaternion.LookRotation(rb.velocity);
+                transform.rotation = Quaternion.Euler(0f, lookRotation.eulerAngles.y, 0f);
+            }
+        }
+    }
+}
