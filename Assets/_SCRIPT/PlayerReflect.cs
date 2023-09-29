ab7eb4ffd62d860d34c28684256f8297687f6109
Tan-Thanakrit <phumpuang_t@su.ac.th>
2023-09-29 14:17:37 +0700

    (add) Player refection

---
+++ Assets/_SCRIPT/PlayerReflect.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReflect : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 lastVelocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        lastVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var speed = lastVelocity.magnitude;
+        var direction = Vector3.Reflect(lastVelocity.normalized, other.contacts[0].normal);
+
+        rb.velocity = direction * Mathf.Max(speed, 0f);
+    }
+    
+}
