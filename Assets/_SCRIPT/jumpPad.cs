b3946f727c038729f8ddcd06c00e551701615266
Monday <mangmalai_p@silpakorn.edu>
2023-11-03 12:13:00 +0700

    (add) Stage 1 +Stage 1 +Light +Prefab Jump Pad and Angle +Kiki aninmation

--- Assets/_SCRIPT/jumpPad.cs
+++ Assets/_SCRIPT/jumpPad.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class jumpPad : MonoBehaviour
 {
-    [Range(0, 100)]
-    public float bouncehight;
 
-    private void OnCollisionEnter(Collision collision)
+    public float jumpForce = 10.0f;
+
+    private void OnTriggerEnter(Collider other)
     {
-        GameObject bouncer = collision.gameObject;
-        Rigidbody rb = bouncer.GetComponent<Rigidbody>();
-        rb.AddForce(Vector3.up * bouncehight);
-
-
+        // Check if the object that entered the trigger is the player or has a specific tag.
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
+            if (playerRigidbody != null)
+            {
+                // Apply a force to the player's Rigidbody to make them jump.
+                playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
     }
-
 }
