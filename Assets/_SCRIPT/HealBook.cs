11dab9de24e2dcbd4c267dc11c1ab1d103217895
Tan-Thanakrit <phumpuang_t@su.ac.th>
2024-03-05 22:35:07 +0700

    (add) Heal Book Prefab

---
+++ Assets/_SCRIPT/HealBook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lean.Pool;
+using UnityEngine;
+
+public class HealBook : MonoBehaviour
+{
+    // Reference to the HealthSystem component of the player
+    private HealthSystem playerHealth;
+
+    private void Start()
+    {
+        // Find and assign the HealthSystem component of the player
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthSystem>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the collided object is the player
+        if (other.CompareTag("Player"))
+        {
+            // Heal the player by 1
+            playerHealth.Heal(1);
+            
+            // Destroy the heal book after it's been picked up
+            LeanPool.Despawn(gameObject);
+        }
+    }
+}
