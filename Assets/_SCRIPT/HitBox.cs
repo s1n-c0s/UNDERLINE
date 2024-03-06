7b94cb923823773ff52b4ca9e898d6d450eb21d4
Tan-Thanakrit <phumpuang_t@su.ac.th>
2024-03-06 11:37:11 +0700

    (update) Hitbox + Vfx active

--- Assets/_SCRIPT/HitBox.cs
+++ Assets/_SCRIPT/HitBox.cs
@@ -1,24 +1,36 @@
+using System;
 using UnityEngine;
 
 public class Hitbox : MonoBehaviour
 {
     [SerializeField] private E_RandomDamages randomDamagesManager;
     [SerializeField] private HealthSystem _healthSystem;
+
+    [Header("Random Settings")]
     [SerializeField] private bool _isRandom;
     [SerializeField] private int _damage;
 
-    private void Awake()
+    [Header("VFX")]
+    [SerializeField] private GameObject _fxWeakpoint;
+
+    private void Start()
     {
+        _fxWeakpoint.SetActive(false);
         if (_damage == 0)
         {
             _isRandom = true;
             _damage = randomDamagesManager.GetUnusedRandomDamage();
         }
+
+        if (_damage >= 3)
+        {
+            _fxWeakpoint.SetActive(true);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (CompareTag("Player") || CompareTag("Shuriken") || CompareTag("Enemy"))
+        if (other.CompareTag("Player") || other.CompareTag("Shuriken") || other.CompareTag("Enemy"))
         {
             _healthSystem.TakeDamage(_damage);
         }
