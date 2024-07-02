fcdccef0c590dde9deb23d1d8525a314fae72f08
Tan-Thanakrit <phumpuang_t@silpakorn.edu>
2024-07-02 15:07:43 +0700

    (fix) Not heal shadow

--- Assets/_SCRIPT/HealBook.cs
+++ Assets/_SCRIPT/HealBook.cs
@@ -8,17 +8,12 @@ public class HealBook : MonoBehaviour
     // Reference to the HealthSystem component of the player
     private HealthSystem playerHealth;
 
-    private void Start()
-    {
-        // Find and assign the HealthSystem component of the player
-        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthSystem>();
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         // Check if the collided object is the player
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !other.GetComponent<ShadowLife>())
         {
+            playerHealth = other.GetComponent<HealthSystem>();
             // Heal the player by 1
             playerHealth.Heal(1);
             
