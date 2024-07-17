9032c756b61b61a8eb346369a6b08e8ae5aed0bc
Tan-Thanakrit <phumpuang_t@silpakorn.edu>
2024-07-17 20:10:56 +0700

    (add) Countdown to fall

--- Assets/_SCRIPT/FallPlatform.cs
+++ Assets/_SCRIPT/FallPlatform.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 public class FallPlatform : MonoBehaviour
 {
     [SerializeField] private bool canFall = true;
-    [SerializeField] private float Cooldown;
+    [SerializeField] private float Countdown = 2f; // Time before the platform falls
+    [SerializeField] private float Cooldown = 5f; // Time before the platform resets
     [SerializeField] private GameObject _platform;
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && canFall)
         {
-            _platform.SetActive(false);
-            canFall = false;
-            StartCoroutine(ResetPlatform());
+            StartCoroutine(CountdownToFall());
         }
+    }
+
+    private IEnumerator CountdownToFall()
+    {
+        yield return new WaitForSeconds(Countdown);
+        _platform.SetActive(false);
+        canFall = false;
+        StartCoroutine(ResetPlatform());
     }
 
     private IEnumerator ResetPlatform()
