653899f465017a17bf6dd2ce0940cd8273a2d631
Tan-Thanakrit <phumpuang_t@silpakorn.edu>
2024-07-20 23:55:32 +0700

    (add) Set active false when helping friends

--- Assets/_SCRIPT/JailObj.cs
+++ Assets/_SCRIPT/JailObj.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Lean.Pool;
 using Unity.Mathematics;
 using UnityEngine;
@@ -11,27 +12,35 @@ public class JailObj : MonoBehaviour
     [Header("VFX")]
     [SerializeField] private ParticleSystem fxUnlock;
     [SerializeField] private ParticleSystem fxSlash;
+    [SerializeField] private List<GameObject> fx_groupLock;
 
     public event Action<JailObj> OnJailUnlock; // Instance event
 
     public bool IsLocked => isLock; // Provide a public read-only property
-
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && isLock)
         {
             isLock = false;
             Door.SetActive(false);
+            foreach (var fx_lock in fx_groupLock)
+            {
+                fx_lock.SetActive(false);
+            }
 
             OnJailUnlock?.Invoke(this); // Notify the GameManager
 
-            if (fxSlash != null && fxUnlock != null)
-            {
-                ParticleSystem _fxUnlock = LeanPool.Spawn(fxSlash, transform.forward, quaternion.identity);
-                LeanPool.Despawn(_fxUnlock, 3f);
-                ParticleSystem fxinit = LeanPool.Spawn(fxSlash, transform.position, Quaternion.identity);
+            /*if (fxSlash != null && fxUnlock != null)
+            {*/
+                Vector3 objPosition = transform.position;
+                objPosition.y += 2f;
+                
+                /*ParticleSystem _fxUnlock = LeanPool.Spawn(fxUnlock, transform.forward, quaternion.identity);
+                LeanPool.Despawn(_fxUnlock, 3f);*/
+                ParticleSystem fxinit = LeanPool.Spawn(fxSlash, objPosition, Quaternion.identity);
                 LeanPool.Despawn(fxinit, 3f);
-            }
+            /*}*/
         }
     }
 }
